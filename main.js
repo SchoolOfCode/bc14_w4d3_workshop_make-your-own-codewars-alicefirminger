a36353024804a1593caeceb6e19171798bd86f1a
alicefirminger <alicefirminger@gmail.com>
2023-04-09 12:27:26 +0100

    wrote some initial tests, not sure about my function yet

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /* 
 👉 Write your kata here!
+Title: Buffy vs. Vampires
+
+Buffy the Vampire Slayer is on a mission to slay as many vampires as she can. Your job is to write a function to help her. 
+
+Each vampire has a different strength represented by the length of their name. Buffy needs one stake per letter in the vampire's name to defeat them. 
+
+Your function should take in a vampire's name as a string and return the number of stakes Buffy needs to defeat them. 
+
+Note: Make sure to handle uppercase and lowercase letters in the vampire's names.
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function vampireSlaying (vampireName) {
+    //your code here
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,43 @@
 //👉 Write your tests below here:
+
+import { test, expect } from "@jest/globals"; 
+
+import {
+    vampireSlaying,
+} from "./main.js";
+
+test ("Check the number of letters in the name Spike"), () => {
+    const vampireName = "Spike";
+    const expected = 5;
+    expect(vampireName).toBe(expected);
+}
+
+test ("Check the number of letters in the name Angelus"), () => {
+    const vampireName = "Angelus";
+    const expected = 7;
+    expect(vampireName).toBe(expected);
+}
+
+test ("Check the number of letters in the name Drusilla"), () => {
+    const vampireName = "Drusilla";
+    const expected = 8;
+    expect(vampireName).toBe(expected);
+}
+
+test ("Check the number of letters in the name Darla"), () => {
+    const vampireName = "Darla";
+    const expected = 5;
+    expect(vampireName).toBe(expected);
+}
+
+test ('Check for upper and lowercase letters'), () => {
+    const 
+}
+
+test ('Check for an empty string')
+
+test ('Check for undefined input')
+
+test ('Check for null input')
+
+test ('Check for names that include numbers or symbols')
