12bbb1393cf69de1db23db509e84da0eb84eaa37
alicefirminger <alicefirminger@gmail.com>
2023-04-09 18:22:30 +0100

    all tests working when correct function is written

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 /* 
-👉 Write your kata here!
-Title: Buffy vs. Vampires
+Title: 👱🏼‍♀️ Buffy vs. Vampires 🧛🏻
 
 Buffy the Vampire Slayer is on a mission to slay as many vampires as she can. Your job is to write a function to help her. 
 
 Each vampire has a different strength represented by the length of their name. Buffy needs one stake per letter in the vampire's name to defeat them. 
 
-Your function should take in a vampire's name as a string and return the number of stakes Buffy needs to defeat them. 
+Your function should take in a vampire's name as a string and return the number of stakes Buffy needs to defeat them. Try to find out how many stakes Buffy needs in total to defeat all the vampires. Happy slaying!
 
+
+
+
+HINT: Your function should be able to check both the number of letters in an individual string AND in an array of strings.
 */
 
-// //👉 Write the function your CodeWarriors will start with below here:
-export function buffyStakes(vampireName) {
-  return 
+
+
+//👇 Get started with the function below 👇
+
+export function buffyStakes(vampireNames) {
+
 }
--- main.test.js
+++ main.test.js
@@ -6,35 +6,34 @@ import { buffyStakes } from './main.js';
 test ("Check the number of letters in the name Spike", () => {
     const vampireName = "Spike";
     expect(buffyStakes(vampireName)).toBe(5);
+    console.log(buffyStakes(vampireName));
 });
 
 test ("Check the number of letters in the name angelus", () => {
     const vampireName = "Angelus";
     expect(buffyStakes(vampireName)).toBe(7);
+    console.log(buffyStakes(vampireName));
 });
 
 test ("Check the number of letters in the name Drusilla", () => {
     const vampireName = "Drusilla";
     expect(buffyStakes(vampireName)).toBe(8);
+    console.log(buffyStakes(vampireName));
 });
 
-test ("Check the number of letters in the name DARLA", () => {
+test ("Check the number of letters in the name Darla", () => {
     const vampireName = "Darla";
     expect(buffyStakes(vampireName)).toBe(5);
+    console.log(buffyStakes(vampireName));
 });
 
 test('Return 0 when the name is an empty string', () => {
     expect(buffyStakes("")).toBe(0);
+    console.log(buffyStakes(""));
 });  
 
-
-   
-
-
-
-// test('should return correct number of stakes for mixed case input', () => {
-//     expect(buffyStakes('Buffy')).toBe(5);
-//     expect(buffyStakes('ANGEL')).toBe(5);
-//     expect(buffyStakes('SpIkE')).toBe(5);
-//     expect(buffyStakes('Willow')).toBe(6);
-//   }); example from chat GPT
+test ('Add up all the letters in the vampire names array', () => {
+    const allVampireNames = ["Spike", "Darla", "Drusilla", "Angelus"]
+    expect(buffyStakes(allVampireNames)).toBe(25); 
+    console.log(buffyStakes(allVampireNames));
+});
